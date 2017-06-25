80f8788506adf115b6dfe39276631288179d1b49
JacekWit <jacek_witkowski@interia.pl>
2017-06-25 16:26:46 +0200

    init

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 'use strict';
 
 
-    
-    
-    $(window).scroll(function() {
-   if($(window).scrollTop() + $(window).height() == $(document).height()) {
-       alert("bottom!");
-    
-    
-    
+$(function () {
+
+
+
+    $(window).scroll(function () {
+            if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+        $.getJSON('http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl', function(data){
+            data.forEach(function(element){
+                console.log(element.name);
+                console.log(element.id);
+                console.log(website.id);
+            });
+        });
+        }
+    });
+
 });
