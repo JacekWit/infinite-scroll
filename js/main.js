dfff082da77537e01eb086b1957f430aa146accd
JacekWit <jacek_witkowski@interia.pl>
2017-06-25 15:46:58 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+    
+    
+    $(window).scroll(function() {
+   if($(window).scrollTop() + $(window).height() == $(document).height()) {
+       alert("bottom!");
+    
+    
+    
+});
